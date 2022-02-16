a17e66339b2a63835711b318b0f01c920233e8a5
Chris <louischrisorona@gmail.com>
2022-02-16 15:01:23 -0800

    Updated App

--- App.vue
+++ App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Brewhaus logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/beer.svg"
+          transition="scale-transition"
+          width="40"
+        />
+        <span class="mr-2">Brewhaus</span>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <Brewhaus />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Brewhaus from './components/Brewhaus';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    Brewhaus
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+      beers: [],
+      // baseURL: 'https://api.punkapi.com/v2/beers',
+    }),
+
+  methods: {
+    async getBeers() {
+      const response = await this.$http.get('https://api.punkapi.com/v2/beers')
+      this.beers = response.data
+    }
+  },
+
+  created() {
+    this.getBeers()
+  }
+};
+</script>
